1f61f60c3d0d217414138a5fcb0bedfda5ea9e6f
Denys Vasylyev <denvasden@gmail.com>
2023-01-04 17:14:26 +0200

    Implement POC

--- App.js
+++ App.js
@@ -1,117 +1,323 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {createNoise3D} from 'simplex-noise';
+import {GLView} from 'expo-gl';
+import {Renderer, TextureLoader, THREE} from 'expo-three';
+import {Alert, PixelRatio, TouchableWithoutFeedback, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// This texture will be immediately read, but it'll load asynchronously
+const textureSphere = new TextureLoader().load(require('./texture-sphere.jpg'));
+const textureIcon1 = new TextureLoader().load(require('./texture-icon-1.png'));
+const textureIcon2 = new TextureLoader().load(require('./texture-icon-2.png'));
+const textureIcon3 = new TextureLoader().load(require('./texture-icon-3.png'));
+const textureIcon4 = new TextureLoader().load(require('./texture-icon-4.png'));
+const textureIcon5 = new TextureLoader().load(require('./texture-icon-5.png'));
+const textureSceneBackground = new TextureLoader().load(
+  require('./texture-scene-background.jpg'),
+);
+const textureStar = new TextureLoader().load(require('./texture-star.png'));
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+global.THREE = global.THREE || THREE;
+
+const glViewStyle = {
+  height: '100%',
+  width: '100%',
+};
+// const noise3D = createNoise3D();
+const viewStyle = {
+  height: '100%',
+  width: '100%',
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const navigationItems = [
+  {
+    name: 'Bitcoin',
+    position: {
+      x: 0,
+      y: -6,
+      z: 368,
+    },
+    texture: textureIcon1,
+  },
+  {
+    name: 'Card',
+    position: {
+      x: -20,
+      y: -4,
+      z: 304,
+    },
+    texture: textureIcon2,
+  },
+  {
+    name: 'Currency',
+    position: {
+      x: 20,
+      y: -4,
+      z: 304,
+    },
+    texture: textureIcon3,
+  },
+  {
+    name: 'Target',
+    position: {
+      x: -28,
+      y: 4,
+      z: 272,
+    },
+    texture: textureIcon4,
+  },
+  {
+    name: 'Digital innovation',
+    position: {
+      x: 28,
+      y: 4,
+      z: 272,
+    },
+    texture: textureIcon5,
+  },
+];
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+let camera = null;
+let context = null;
+let trackedObjects = [];
+let pointer = null;
+let raycaster = null;
+
+export default function App() {
+  function handlePress(event) {
+    const pointerX =
+      (event.nativeEvent.locationX /
+        (context.drawingBufferWidth / PixelRatio.get())) *
+        2 -
+      1;
+    const pointerY =
+      -(
+        event.nativeEvent.locationY /
+        (context.drawingBufferHeight / PixelRatio.get())
+      ) *
+        2 +
+      1;
+    pointer.set(pointerX, pointerY);
+
+    raycaster.setFromCamera(pointer, camera);
+
+    const intersects = raycaster.intersectObjects(trackedObjects, false);
+
+    if (intersects.length) {
+      const name = intersects[0].object.name;
+      const navigationItem = navigationItems.find(
+        navigationItem => navigationItem.name === name,
+      );
+
+      if (navigationItem) {
+        Alert.alert(`Interaction will lead to "${name}" screen`);
+      }
+    } else {
+      Alert.alert('Not a sphere!');
+    }
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={viewStyle}>
+        <GLView onContextCreate={onContextCreate} style={glViewStyle} />
+      </View>
+    </TouchableWithoutFeedback>
   );
-};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  async function onContextCreate(gl) {
+    context = gl;
 
-export default App;
+    camera = new THREE.PerspectiveCamera(
+      45,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      1,
+      1024,
+    );
+    camera.position.set(0, 0, 512);
+
+    const ambientLight = new THREE.AmbientLight('#ffffff', 1);
+    ambientLight.position.set(0, 32, 32);
+
+    const directionalLight1 = new THREE.DirectionalLight('#ffffff', 1);
+    directionalLight1.position.set(16, 32, 32);
+    const directionalLight2 = new THREE.DirectionalLight('#ffffff', 2);
+    directionalLight2.position.set(-16, -32, -32);
+
+    const renderer = new Renderer({gl});
+    renderer.setClearColor('#000000');
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    const scene = new THREE.Scene();
+    scene.background = textureSceneBackground;
+    setSceneBackground({
+      backgroundImage: {
+        height: 2160,
+        width: 3840,
+      },
+      gl,
+      scene,
+    });
+
+    const starsCount = 256;
+    const starsGeometry = new THREE.BufferGeometry();
+    const starsMaterial = new THREE.PointsMaterial({
+      alphaMap: textureStar,
+      blending: THREE.AdditiveBlending,
+      depthWrite: false,
+      opacity: 0.6,
+      size: 4.6,
+      transparent: true,
+    });
+
+    const positions = new Float32Array(starsCount * 3);
+    const starsVectors = [];
+
+    for (let i = 0; i < starsCount; i++) {
+      const vector = getStarVector();
+
+      vector.startX = vector.x;
+      vector.startY = vector.y;
+      vector.startZ = vector.z;
+      vector.velocity = THREE.MathUtils.randInt(32, 512);
+
+      starsVectors.push(vector);
+    }
+
+    let positionIndex = 0;
+
+    for (let i = 0, l = starsCount; i < l; i++) {
+      positions[positionIndex++] = starsVectors[i].x;
+      positions[positionIndex++] = starsVectors[i].y;
+      positions[positionIndex++] = starsVectors[i].z;
+    }
+
+    starsGeometry.setAttribute(
+      'position',
+      new THREE.BufferAttribute(positions, 3),
+    );
+
+    const sphereGeometry = new THREE.IcosahedronGeometry(32, 16);
+    const sphereMaterial = new THREE.MeshPhongMaterial({map: textureSphere});
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphere.name = 'sphere';
+    sphere.position.z = 128;
+    trackedObjects.push(sphere);
+
+    const stars = new THREE.Points(starsGeometry, starsMaterial);
+
+    raycaster = new THREE.Raycaster();
+    pointer = new THREE.Vector2();
+
+    const orbit = new THREE.Group();
+    const clock = new THREE.Clock();
+
+    scene.add(ambientLight);
+    scene.add(directionalLight1);
+    scene.add(directionalLight2);
+    scene.add(sphere);
+    scene.add(stars);
+
+    const navigationItemsMeshes = navigationItems.map(navigationItem => {
+      const geometry = new THREE.CircleGeometry(10, 32);
+      const material = new THREE.MeshBasicMaterial({
+        map: navigationItem.texture,
+        transparent: true,
+      });
+      const mesh = new THREE.Mesh(geometry, material);
+
+      mesh.name = navigationItem.name;
+      mesh.position.x = navigationItem.position.x;
+      mesh.position.y = navigationItem.position.y;
+      mesh.position.z = navigationItem.position.z;
+
+      return mesh;
+    });
+
+    navigationItemsMeshes.forEach(navigationItemMesh => {
+      trackedObjects.push(navigationItemMesh);
+      scene.add(navigationItemMesh);
+    });
+
+    render();
+
+    function render() {
+      animateGeometry();
+      animateStars();
+
+      const time = clock.getElapsedTime();
+
+      navigationItemsMeshes.forEach((navigationItemMesh, index) => {
+        navigationItemMesh.position.y += Math.cos(time) * 0.02;
+      });
+
+      renderer.render(scene, camera);
+      requestAnimationFrame(render);
+
+      gl.flush();
+      gl.endFrameEXP();
+    }
+
+    function animateGeometry() {
+      sphere.rotation.y += 0.004;
+    }
+
+    function animateStars() {
+      starsVectors.forEach(vector => {
+        vector.x += (0 - vector.x) / vector.velocity;
+        vector.y += (0 - vector.y) / vector.velocity;
+        vector.z += (0 - vector.z) / vector.velocity;
+        vector.velocity -= 0.2;
+
+        if (
+          vector.x <= 4 &&
+          vector.x >= -4 &&
+          vector.z <= 4 &&
+          vector.z >= -4
+        ) {
+          vector.x = vector.startX;
+          vector.y = vector.startY;
+          vector.z = vector.startZ;
+          vector.velocity = THREE.MathUtils.randInt(32, 512);
+        }
+      });
+
+      const positions = stars.geometry.attributes.position.array;
+      let positionIndex = 0;
+
+      for (let i = 0, l = starsCount; i < l; i++) {
+        positions[positionIndex++] = starsVectors[i].x;
+        positions[positionIndex++] = starsVectors[i].y;
+        positions[positionIndex++] = starsVectors[i].z;
+      }
+
+      stars.geometry.attributes.position.needsUpdate = true;
+
+      stars.rotation.z += 0.004;
+    }
+  }
+}
+
+function getStarVector(radius = 128) {
+  let theta = 2 * Math.PI * Math.random();
+  let phi = Math.acos(2 * Math.random() - 1);
+  let dx = radius * Math.sin(phi) * Math.cos(theta);
+  let dy = radius * Math.sin(phi) * Math.sin(theta);
+  let dz = radius * Math.cos(phi);
+
+  return new THREE.Vector3(dx, dy, dz);
+}
+
+function setSceneBackground({backgroundImage, gl, scene}) {
+  if (scene.background) {
+    const factor =
+      backgroundImage.width /
+      backgroundImage.height /
+      (gl.drawingBufferWidth / gl.drawingBufferHeight);
+
+    scene.background.offset.x = factor > 1 ? (1 - 1 / factor) / 2 : 0;
+    scene.background.offset.y = factor > 1 ? 0 : (1 - factor) / 2;
+
+    scene.background.repeat.x = factor > 1 ? 1 / factor : 1;
+    scene.background.repeat.y = factor > 1 ? 1 : factor;
+  }
+}
